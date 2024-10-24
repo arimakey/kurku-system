7bd0b5d1249c8bee8f1bc1b59d66fc9472ba628c
Jacito12 <jgarciaa@unitru.edu.pe>
2024-10-24 16:26:09 -0500

    Obtener las url de las imagenes ingresando una coordenada y fechas

--- modules/earth_images.py
+++ modules/earth_images.py
@@ -1,34 +1,43 @@
 import ee
 
-def get_image(longitud, latitud):
-    ee.Authenticate()
-    ee.Initialize(project='prueba-imagen-satelital')
+ee.Authenticate()
+ee.Initialize(project='prueba-imagen-satelital')
 
-    # Crea un punto de interés (POI)
-    poi = ee.Geometry.Point([longitud, latitud])
+def obtener_urls_imagenes(longitud=-70.4967607, latitud=-12.0545491, fecha_inicio='2024-01-01', fecha_fin='2024-10-23'):
+    roi = ee.Geometry.Rectangle([longitud-0.03, latitud-0.03, longitud+0.03, latitud+0.03])
 
-    # Define una región de interés (ROI)
-    roi = ee.Geometry.Rectangle([longitud - 0.015, latitud - 0.015, longitud + 0.015, latitud + 0.015])
-
-    # Filtra la colección para obtener imágenes recientes del ROI
     imagenes = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
         .filterBounds(roi) \
-        .filterDate('2017-03-28', '2024-09-28') \
-        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 5))  # Filtrar por nubes
+        .filterDate(fecha_inicio, fecha_fin) \
+        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 30))
 
-    imagen = imagenes.median()
+    lista_imagenes = imagenes.toList(imagenes.size()).getInfo()
+    
+    urls_imagenes = []
+    
+    for imagen_info in lista_imagenes:
+        id_imagen = imagen_info['id']
+        imagen = ee.Image(id_imagen)
+        
+        imagenrgb = imagen.visualize(**{
+            'min': 0,
+            'max': 2800,
+            'bands': ['B4', 'B3', 'B2']
+        })
+        
+        try:
+            url = imagenrgb.getThumbURL({
+                'region': roi,
+                'scale': 7,
+                'format': 'jpg'
+            })
+            urls_imagenes.append(url)
+        except Exception as e:
+            print(f"Error generando URL para la imagen {id_imagen}: {e}")
+    
+    return urls_imagenes
 
-    # Visualizar la imagen con las bandas RGB de Sentinel-2
-    imagenrgb = imagen.visualize(**{
-        'min': 0,
-        'max': 2000,
-        'bands': ['B4', 'B3', 'B2']
-    })
+urls = obtener_urls_imagenes()
 
-    url = imagenrgb.getThumbURL({
-        'region': roi,
-        'dimensions': 800,
-        'format': 'jpg'
-    })
-
-    return url
+for url in urls:
+    print(url)
